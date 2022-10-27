872f95dbc44445fe89f34e7ca7e96c64f9eebff9
Junwei VM <jx2388@nyu.edu>
2022-10-28 05:29:22 +1100

    add benchmark

--- Experiment/convert.py
+++ Experiment/convert.py
@@ -4,14 +4,11 @@ for root, subdirs, files in os.walk(os.getcwd()):
     for file in files:
         if file.endswith(".intohylo") and not file.startswith("._"):
             with open(root + "/"+file, "r") as f:
-                text = f.readlines()[1]
-            text = text.replace("~ ", "~")
-            text = text.replace("[r1", "[")
-            text = text.replace("<r1", "<")
-            # for lwb s4
+                text = f.readlines()[0]
+            text = text.replace("-", "~")
             text = text.replace("box", "[]") 
             text = text.replace("dia", "<>") 
-            text = text.replace("v", "|") 
+            text = text.replace(";", "|") 
             with open(root+file.replace(".intohylo", ".hf"), "w") as f:
                 f.write(text)
             text = text.replace("true", "$true")
